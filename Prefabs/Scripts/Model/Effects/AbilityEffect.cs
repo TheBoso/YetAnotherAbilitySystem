9b558e88f5a1ac9616b3d2ce3b886e0d0daf573a
Dom <domenicomandica2000@gmail.com>
2025-02-09 00:51:21 +1030

    fix up directories

--- Prefabs/Scripts/Model/Effects/AbilityEffect.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using UnityEngine;
-using YAAS;
-
-public abstract class AbilityEffect : ScriptableObject
-{
- public abstract IEnumerator PerformEffect(AbilityCaster caller);
-}
